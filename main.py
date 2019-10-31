9a89fb5a8cb8eafe5a164cd0e8037a2d0bab0665
hani-mansour <hani.mansour.1990@gmail.com>
2019-10-31 15:34:28 +0100

    hash function and session added

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from model import User, db
+import hashlib, uuid
 app = Flask(__name__)
 
 db.create_all() # erstellt neue Datenbank Tabelle(n)
 
 @app.route("/")
 def index():
-    email_address = request.cookies.get("email")
-    if email_address:
-        user = db.query(User).filter_by(email=email_address).first()
+    session_token = request.cookies.get("session_token")
+    if session_token:
+        user = db.query(User).filter_by(session_token=session_token).first()
     else:
         user = None
     #print(user.name)
@@ -18,15 +19,27 @@ def index():
 def login():
     name = request.form.get("user-name")
     email = request.form.get("user-email")
+    password = request.form.get("user-password")
+    hashed_pw = hashlib.sha256(password.encode()).hexdigest()
+    print(hashed_pw)
 
     #neues Objekt von Typ User (Model)
-    user = User(name=name, email=email)
-    db.add(user)
-    db.commit()
-    #cookie
-    response = make_response(redirect(url_for('index')))
-    response.set_cookie("email", email)
-    return response
+    user = db.query(User).filter_by(email=email).first()
+    if not user:
+        user = User(name=name, email=email, password=hashed_pw)
+        db.add(user)
+        db.commit()
+    if hashed_pw != user.password:
+        return  "Wrong Password! Try again"
+    elif hashed_pw == user.password:
+        session_token = str(uuid.uuid4())
+        user.session_token = session_token
+        db.add(user)
+        db.commit()
+        #cookie
+        response = make_response(redirect(url_for('index')))
+        response.set_cookie("session_token", session_token, httponly=True, samesite='Strict')
+        return response
 
 if __name__ == '__main__':
     app.run()
--- model.py
+++ model.py
@@ -7,3 +7,5 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    session_token = db.Column(db.String)
